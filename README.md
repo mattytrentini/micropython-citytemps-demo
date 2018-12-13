edb1813ab21cddd1d7de3645b5bf8dd861333787
Matt Trentini <matt.trentini@gmail.com>
2018-12-13 15:11:46 +1100

    A few tidy-ups. Removed a number of magic numbers, correctly closed requests socket, added LICENSE, added helper function to centre text.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 
-Configure
+## Installation
+
+Copy contents of `src` to the root of a MicroPython-enabled device.
+The device should have an SSD1306-powered I2C display and be wifi-capable.
+Tested on a Wemos with OLED display (as pictured).
+
+## Configure
+
+A configuration file is supplied in JSON format. Contents include...
--- src/main.py
+++ src/main.py
@@ -5,69 +5,75 @@ import utime as time
 import urequests as requests
 import ssd1306
 
-# Initialise OLED
-i2c = I2C(scl=Pin(4), sda=Pin(5))
-screen_width, screen_height = 128, 64
-oled = ssd1306.SSD1306_I2C(screen_width, screen_height, i2c)
-oled.fill(0)
-
-# Display a heading
-text_height = 8
-heading_padding = 10
-heading = '-= City Temps =-'
-oled.text(heading,
-          (screen_width - len(heading) * text_height) // 2,
-          heading_padding)
-oled.show()
-
 # Read config
 with open('config.json') as f:
     config = json.load(f)
+
+# Initialise OLED
+i2c = I2C(scl=Pin(config['scl_pin']), sda=Pin(config['sda_pin']))
+oled = ssd1306.SSD1306_I2C(128, 64, i2c)
+oled.fill(0)
+
+def text_horiz_centred(fb, text, y, char_width=8):
+    ''' Helper to display text horizontally centred '''
+    fb.text(text, (fb.width - len(text) * char_width) // 2, y)
+
+# Display a heading
+char_height = 8
+text_horiz_centred(oled, '-= City Temps =-', 10)
+oled.show()
 
 # Establish network connection
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(config['ssid'], config['ssid_password'])
 
-connecting = 'Connecting to'
-oled.text(connecting,
-          (screen_width - len(connecting) * text_height) // 2,
-          text_height * 6)
-oled.text(config['ssid'],
-          (screen_width - len(config['ssid']) * text_height) // 2,
-          text_height * 7)
+# Display a "Connecting to network" message
+text_horiz_centred(oled, 'Connecting to', char_height * 6)
+text_horiz_centred(oled, config['ssid'], char_height * 7)
 oled.show()
 
 while not wlan.isconnected():
-    # TODO: Should timeout
+    # TODO: Should timeout. How to handle timeout? Retry connect?
     time.sleep(1)
 
 while True:
-    print('Query Weather Map')
     # Query OpenWeatherMap for city temperatures
     api_root = 'https://api.openweathermap.org/data/2.5'
-    r = requests.get(api_root + '/group?id={},{}&units=metric&APPID={}'.format(config['city1_id'],
-                                                                               config['city2_id'],
-                                                                               config['appid']))
-    # TODO: Check response code
+    api_params = '/group?id={},{}&units=metric&APPID={}'
+    r = requests.get(api_root + api_params.format(config['city1_id'],
+                                                  config['city2_id'],
+                                                  config['appid']))
+    # TODO Check return code. How to hand if not 200?
+    # if r.status_code == 200:
+
     # Parse JSON for city names and temperatures
     city_temps = [(city['name'], city['main']['temp']) for city in r.json()['list']]
-    start_time = time.ticks_ms()
+    r.close()
+    start = time.ticks_ms()
 
+    # Fill top half of display white, bottom half black
     oled.fill(1)
-    oled.fill_rect(0, 31, 128, 62, 0)
+    oled.fill_rect(0, oled.height // 2, oled.width, oled.height, 0)
+
+    # Display city names
     oled.text(city_temps[0][0], 5, 5, 0)
     oled.text(city_temps[1][0], 5, 36)
-    oled.line(0, 63, 128, 63, 1)
 
+    # Display city temperatures
     oled.text('{:4.1f}'.format(city_temps[0][1]), 90, 11, 0)
     oled.text('{:4.1f}'.format(city_temps[1][1]), 90, 42, 1)
 
+    # Draw full 'progress bar' on bottom line of display
+    oled.line(0, oled.height - 1, oled.width, oled.height - 1, 1)
+
     oled.show()
 
-    while time.ticks_ms() - start_time < config['query_interval_sec'] * 1000:
-        # Update progress, sleep
+    # Sleep, update progress
+    while time.ticks_diff(time.ticks_ms(), start) < config['query_interval_sec'] * 1000:
         time.sleep(config['update_interval_sec'])
-        next_query_percent = (time.ticks_ms() - start_time) / (config['query_interval_sec'] * 1000)
-        oled.line(128 - int(128 * next_query_percent), 63, 128, 63, 0)
+        next_query_percent = (time.ticks_diff(time.ticks_ms(), start) / (config['query_interval_sec'] * 1000)
+        oled.line(oled.width - int(oled.width * next_query_percent), oled.height - 1,
+                  oled.width, oled.height - 1,
+                  0)
         oled.show()
